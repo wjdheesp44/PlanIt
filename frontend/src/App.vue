e46bdc77837322569b18f1d834c66fd5429b58eb
Heesu Jeong <huissppea@gmail.com>
2025-12-08 11:14:54 +0900

    chore: initial frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header />
+  <RouterView></RouterView>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "./components/layout/Header.vue";
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <RouterLink to="#"><img src="@/assets/img/logo.png" alt="홈 버튼" /></RouterLink>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">홈</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+header {
+  border: 1px solid black;
+
+  ul {
+    display: flex;
+    justify-content: space-around;
+  }
+}
+</style>
