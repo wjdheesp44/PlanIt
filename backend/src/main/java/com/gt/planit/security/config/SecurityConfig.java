d1e50b0bf859eb6743dbfd4ecd6f4f97dce7e9aa
Yoonjung Kong <yjk1800@gmail.com>
2025-12-11 13:45:43 +0900

    #4 feat: cors 오류 해결, url 접두사 추가

--- backend/src/main/java/com/gt/planit/security/config/SecurityConfig.java
+++ backend/src/main/java/com/gt/planit/security/config/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,23 +31,18 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .formLogin(form -> form.disable())
-//                .httpBasic(basic -> basic.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/login").permitAll()
-//                        .anyRequest().authenticated()
-//                );
-//
-//        http.addFilterAt(jwtAuthenticationFilter(),
-//                UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -61,9 +61,12 @@ public class SecurityConfig {
         );
 
         http.csrf(csrf -> csrf.disable());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
-        http.addFilter(loginFilter)
-            .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.formLogin(form -> form.disable());
+
+        http.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
