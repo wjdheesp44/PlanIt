b1cb6c0868f262cc1964f6fd4a15f21f17a02e99
정희수 <huissppea@gmail.com>
2025-12-19 17:41:16 +0900

    #17 feat: 스팟 세부 페이지 구현

--- backend/src/main/java/com/gt/planit/domain/spot/model/service/SpotService.java
+++ backend/src/main/java/com/gt/planit/domain/spot/model/service/SpotService.java
@@ -1,9 +1,11 @@
 package com.gt.planit.domain.spot.model.service;
 
+import com.gt.planit.domain.spot.model.dto.SpotDetailResDto;
 import com.gt.planit.domain.spot.model.dto.SpotRes;
 import com.gt.planit.domain.spot.model.dto.SpotSearchCondition;
 import com.gt.planit.global.response.PageRes;
 
 public interface SpotService {
-    PageRes<SpotRes> searchSpots(SpotSearchCondition condition, int page, int size);
+    PageRes<SpotRes> searchSpots(SpotSearchCondition condition, int page, int size, Long userId);
+    SpotDetailResDto searchSpotsById(Long spotId, Long userId);
 }
